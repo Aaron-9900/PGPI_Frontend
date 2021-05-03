246aafa2a48fb8edcad1369febfa0c869f2972f5
Aaron Hoffman <aaronhoffman1999@gmail.com>
2021-05-03 12:33:06 +0200

    Add backend uri

--- src/services/apiconfig.ts
+++ src/services/apiconfig.ts
@@ -4,6 +4,6 @@ export interface ApiConfig {
 }
 
 export const API_CONFIG: ApiConfig = {
-  baseUrl: "http://localhost:8888/PGPI/api/backend/",
+  baseUrl: "http://pgpi-backend.herokuapp.com/PGPI/api/backend/",
   timeout: 20000,
 }
